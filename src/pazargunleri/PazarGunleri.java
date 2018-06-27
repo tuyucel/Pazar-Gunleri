6f508d8217330471ecc1fd170fd778c2219aa385
Turgay yücel <tuyucel@stu.okan.edu.tr>
2018-06-27 15:44:32 +0200

    update

--- src/pazargunleri/PazarGunleri.java
+++ src/pazargunleri/PazarGunleri.java
@@ -13,29 +13,27 @@ public class PazarGunleri {
     }
 
     public static void main(String[] args) {
-
         int gun = 1;
         int ilkYil = 1900, ikinciYil = 2000;
-        int[] ayKacGun;
-        ayKacGun = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        byte[] ayKacGun;
+        ayKacGun = new byte[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         for (int yil = ilkYil; yil <= ikinciYil; yil++) {
             if (artikYil(yil)) {
                 ayKacGun[1] = 29;
             } else {
                 ayKacGun[1] = 28;
             }
-            int ay = 0;
-            while (ay < ayKacGun.length) {
+            byte ay = 0;
 
-                gun += ayKacGun[ay];
+            while (ay < ayKacGun.length) {
+                gun += (byte) ayKacGun[ay];
                 if (gun % 7 == 0 && ay == 11) {
                     System.out.println("01." + (ay - 10) + "." + (yil + 1) + " - Pazar");
                 } else if (gun % 7 == 0) {
-                    System.out.println("01." + (ay + 2) + "." + yil + " - Pazar");
+                    System.out.println("01." + (ay+2) + "." + yil + " - Pazar");
                 }
                 ay++;
             }
         }
     }
-
 }
